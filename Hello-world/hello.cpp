fc280add8d5b2bc01cd3e2bf1005649710a93160
Acrotwit <118976236+Acrotwit@users.noreply.github.com>
2025-05-19 00:45:41 +0100

    finished

--- Hello-world/hello.cpp
+++ Hello-world/hello.cpp
@@ -2,45 +2,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <limits>
 using namespace std;
 
 //prompt for template file
 int templateSelect() { //function to prompt user for template file
     int templatenumber;
-    while (true) {
-        cout << "Select a template: " << endl; // Type a number and press enter
-        cin >> templatenumber; // Get user input from the keyboard
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cout << "Invalid input. Please enter a number." << endl;
-            continue;
-        }
-        cout << "Your number is: " + to_string(templatenumber) + " ";
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        return templatenumber;
-    }
+    cout << "Select a template: " << endl; // Type a number and press enter
+    cin >> templatenumber; // Get user input from the keyboard
+    cout << "Your number is: " + to_string(templatenumber) + " ";
+    return templatenumber;
 }
 
 // function to read the template file from current directory and return the content as a string
 string filereader(int x) {
     // variable to store the content of the file
-    string webpage; 
-    // variable to store the name of the file
+	string webpage; 
+	// variable to store the name of the file
     string filename = ("template" + to_string(x) + ".html");
-    // open the file in read mode
+	// open the file in read mode
     ifstream file(filename);
-    // check if the file is open
+	// check if the file is open
     if (file.is_open()) {
         string line;
         while (getline(file, line)) {
-            // read each line of the file and append it to the webpage variable
+			// read each line of the file and append it to the webpage variable
             webpage += line + "\n";
         }
-    } else {
+    } 
+    else {
         cout << "Unable to open file or does not exist" << endl;
-        return "error";
+		return "error";
     }
     cout << webpage << endl;
     file.close();
@@ -61,13 +52,10 @@ StudentDetails studentdetailsinput() {
     StudentDetails details;
     cout << "Enter the image path of student photo: " << endl;
     cin >> details.image;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     cout << "Enter the name of the student: " << endl;
     cin >> details.name;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     cout << "Enter the ID of the student: " << endl;
     cin >> details.id;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     return details;
 }
@@ -75,28 +63,30 @@ StudentDetails studentdetailsinput() {
 string filewriter(string webpage, string x, string y, string z) {
     size_t img_pos = webpage.find("src=\"\"");
     if (img_pos != string::npos) {
-        webpage.replace(img_pos, 7, "src=\"" + x + "\"");
+            webpage.replace(img_pos, 7, "src=\"" + x + "\"");
+        }
+
+        size_t name_pos = webpage.find("Student_Name");
+        if (name_pos != string::npos) {
+            webpage.replace(name_pos, 12, y);
+        }
+
+        size_t id_pos = webpage.find("Student_Id");
+        if (id_pos != string::npos) {
+            webpage.replace(id_pos, 10, z);
+        }
+
+        return webpage;
     }
 
-    size_t name_pos = webpage.find("Student_Name");
-    if (name_pos != string::npos) {
-        webpage.replace(name_pos, 12, y);
+    string webpagecreator(string webpage) {
+		ofstream file("webpage.html");
+		file << webpage;
+		file.close();
+        return webpage;
     }
 
-    size_t id_pos = webpage.find("Student_Id");
-    if (id_pos != string::npos) {
-        webpage.replace(id_pos, 10, z);
-    }
 
-    return webpage;
-}
-
-string webpagecreator(string webpage) {
-    ofstream file("webpage.html");
-    file << webpage;
-    file.close();
-    return webpage;
-}
 
 // function to write the changes to a new file
 
@@ -109,22 +99,15 @@ int main() {
         int templatenumber = templateSelect();
 
         // check if the template file is correct for the user
-        confirmation = 0; // just assign, don't redeclare
+        confirmation = 0;
 
         while (confirmation != 1) {
             cout << "is this the template file you want to use, press 1 for yes" << endl;
             cin >> confirmation;
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout << "Invalid input. Please enter a number." << endl;
-                confirmation = 0;
-                continue;
-            }
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             if (confirmation == 1) {
                 cout << "You have selected the template file " + to_string(templatenumber) << endl;
-            } else {
+            } 
+            else {
                 cout << "Please select a different template file" << endl;
                 templatenumber = templateSelect();
             }
@@ -143,14 +126,6 @@ int main() {
         cout << webpage << endl;
         cout << "Do you want to save the changes made to the file, press 1 for yes" << endl;
         cin >> confirmation;
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            cout << "Invalid input. Please enter a number." << endl;
-            confirmation = 0;
-            continue;
-        }
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         if (confirmation == 1) {
             // write the changes to a new file
@@ -161,6 +136,11 @@ int main() {
             cout << "The file is named webpage.html" << endl;
             cout << "The file is located in the same directory as this program" << endl;
             cout << "Please check the file for the changes made" << endl;
+        }
+        else
+        {
+			cout << "The file has not been created" << endl;
+			cout << "please start over" << endl;
         }
     }
 }
