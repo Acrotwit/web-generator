347abd6588ddf4b83a14400789acc2cd8c729d7d
Acrotwit <118976236+Acrotwit@users.noreply.github.com>
2025-05-18 05:07:38 +0100

    Add template2.html and refactor file reading logic
    Updated project files to include a new HTML template,
    template2.html, and modified filters to categorize it
    alongside template1.html. Refactored the templateSelect()
    function for clarity and introduced filereader(int x)
    to handle reading the selected template file's content.

--- Hello-world/hello.cpp
+++ Hello-world/hello.cpp
@@ -5,53 +5,64 @@
 using namespace std;
 
 //prompt for template file
-int templateSelect(){ //function to prompt user for template file
-int templatenumber;
-cout << "Select a template: " << endl; // Type a number and press enter
-cin >> templatenumber; // Get user input from the keyboard
-cout << "Your number is: " + to_string(templatenumber) + " ";
-return templatenumber;
-	}
+int templateSelect() { //function to prompt user for template file
+    int templatenumber;
+    cout << "Select a template: " << endl; // Type a number and press enter
+    cin >> templatenumber; // Get user input from the keyboard
+    cout << "Your number is: " + to_string(templatenumber) + " ";
+    return templatenumber;
+}
 
 // function to read the template file from \resources folder and return the content as a string
-
-string templateRead(int templatenumber) {
-	string webpage;
-	if (templatenumber == 1) {
-		ifstream file("resources/template1.txt");
-	}
-	for (getline(file, webpage)) {
-		cout << webpage << endl; // print the content of the file to the console
-	}
-
+string filereader(int x) {
+    // variable to store the content of the file
+	string webpage; 
+	// variable to store the name of the file
+    string filename = ("template" + to_string(x) + ".html");
+	// open the file in read mode
+    ifstream file(filename);
+	// check if the file is open
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+			// read each line of the file and append it to the webpage variable
+            webpage += line + "\n";
+        }
+    } else {
+        cout << "Unable to open file or does not exist" << endl;
+    }
+    cout << webpage << endl;
+    file.close();
+    
+    return webpage;
 }
+
+
+
 
 // function to write the changes to a new file
 
-	// prompt user for changes they wish to make to the file
+// prompt user for changes they wish to make to the file
 
 int main() {
-	// call the function to prompt user for template file
-	int templatenumber = templateSelect();
-	// check if the template file is correct for the user
-	int confirmation = 0;
+    // call the function to prompt user for template file
+    int templatenumber = templateSelect();
+    // check if the template file is correct for the user
+    int confirmation = 0;
 
-	while (confirmation != 1) {
-		cout << "is this the template file you want to use ?" << endl;
-		cin >> confirmation;
-		if (confirmation == 1) {
-			cout << "You have selected the template file " + to_string(templatenumber) << endl;
-		}
-		else {
-			cout << "Please select a different template file" << endl;
-			templatenumber = templateSelect();
-		}
-	}
+    while (confirmation != 1) {
+        cout << "is this the template file you want to use ?" << endl;
+        cin >> confirmation;
+        if (confirmation == 1) {
+            cout << "You have selected the template file " + to_string(templatenumber) << endl;
+        } else {
+            cout << "Please select a different template file" << endl;
+            templatenumber = templateSelect();
+        }
+    }
+    // call the function to read the template file and return the content as a string
+    filereader(templatenumber);
 }
- webpage = templateRead(templatenumber); // read the template file and return the content as a string
-
-
-
-
+// read the template file and return the content as a string
 
 // create a new file with the changes made by the user
